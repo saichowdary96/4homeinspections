891cdce4d886038de816733c0d916e8f15a28737
saichowdary96 <saichowdary8496@gmail.com>
2026-06-28 12:39:49 +0530

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,5 +1,6 @@
 import type { MetadataRoute } from "next";
 import { siteConfig } from "@/data/site";
+export const dynamic = "force-static";
 
 export default function robots(): MetadataRoute.Robots {
   const base = siteConfig.url.replace(/\/$/, "");
