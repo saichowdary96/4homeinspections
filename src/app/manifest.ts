849ab0daa0e71959793978532b2068bb8dd18871
saichowdary96 <saichowdary8496@gmail.com>
2026-07-08 18:37:53 +0530

    Update manifest.ts

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -13,11 +13,11 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#0f172a",
 
     icons: [
-      {
-        src: "/favicon.ico",
-        sizes: "any",
-        type: "image/x-icon",
-      },
-    ],
+  {
+    src: "/icon.svg",
+    sizes: "any",
+    type: "image/svg+xml",
+  },
+],
   };
 }
