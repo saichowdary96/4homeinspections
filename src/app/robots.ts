0b54c2e7645b1b332dfc8c34dd00a8b3b6f0418b
saichowdary96 <saichowdary8496@gmail.com>
2026-07-08 16:03:03 +0530

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,14 +1,29 @@
 import type { MetadataRoute } from "next";
 import { siteConfig } from "@/data/site";
+
 export const dynamic = "force-static";
 
 export default function robots(): MetadataRoute.Robots {
   const base = siteConfig.url.replace(/\/$/, "");
+
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+      {
+        userAgent: "Googlebot",
+        allow: "/",
+      },
+      {
+        userAgent: "Bingbot",
+        allow: "/",
+      },
+    ],
+
     sitemap: `${base}/sitemap.xml`,
+
+    host: base,
   };
 }
