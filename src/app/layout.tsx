dc067f0c7fbfbdbfe768e56753f49325f5b31a28
saichowdary96 <saichowdary8496@gmail.com>
2026-07-10 04:05:57 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import "./globals.css";
 import { Header } from "@/components/layout/Header";
 import { Footer } from "@/components/layout/Footer";
 import { FloatingCta } from "@/components/layout/FloatingCta";
-import { GoogleAnalytics } from "@next/third-parties/google";
 import { siteConfig } from "@/data/site";
 import Script from "next/script";
 
